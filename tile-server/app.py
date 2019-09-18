8f3a89d617c90823d3f5937c26c4def064aba1c7
David Lacroix <dlacroix44@gmail.com>
2019-09-18 21:40:33 +0200

    Initial docker compose setup

--- tile-server/app.py
+++ tile-server/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route("/")
+@app.route("/")
 def index():
     return "<h1>I shall serve you tiles</h1>" 
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80)
